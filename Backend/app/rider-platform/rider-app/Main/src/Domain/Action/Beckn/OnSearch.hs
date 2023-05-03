dff2b355a87d9d9c1263622fc6e0affe7ef7a852
Alexander Korovin <alexander.korovin@metalamp.io>
2023-05-03 22:14:07 +0400

    Refactor fare policy and fare parameters

--- Backend/app/rider-platform/rider-app/Main/src/Domain/Action/Beckn/OnSearch.hs
+++ Backend/app/rider-platform/rider-app/Main/src/Domain/Action/Beckn/OnSearch.hs
@@ -16,6 +16,7 @@ module Domain.Action.Beckn.OnSearch
   ( DOnSearchReq (..),
     ProviderInfo (..),
     EstimateInfo (..),
+    DEstimate.FareRange (..),
     QuoteInfo (..),
     QuoteDetails (..),
     OneWayQuoteDetails (..),
@@ -78,20 +79,20 @@ data EstimateInfo = EstimateInfo
     totalFareRange :: DEstimate.FareRange,
     descriptions :: [Text],
     estimateBreakupList :: [EstimateBreakupInfo],
-    nightShiftRate :: Maybe NightShiftInfo,
+    nightShiftInfo :: Maybe NightShiftInfo,
     waitingCharges :: Maybe WaitingChargesInfo,
     driversLocation :: [LatLong]
   }
 
 data NightShiftInfo = NightShiftInfo
-  { nightShiftMultiplier :: Maybe Centesimal,
-    nightShiftStart :: Maybe TimeOfDay,
-    nightShiftEnd :: Maybe TimeOfDay
+  { nightShiftCharge :: Money,
+    oldNightShiftCharge :: Centesimal,
+    nightShiftStart :: TimeOfDay,
+    nightShiftEnd :: TimeOfDay
   }
 
-data WaitingChargesInfo = WaitingChargesInfo
-  { waitingTimeEstimatedThreshold :: Maybe Seconds,
-    waitingChargePerMin :: Maybe Money
+newtype WaitingChargesInfo = WaitingChargesInfo
+  { waitingChargePerMin :: Maybe Money
   }
 
 data EstimateBreakupInfo = EstimateBreakupInfo
@@ -186,17 +187,17 @@ buildEstimate requestId providerInfo now _searchRequest EstimateInfo {..} = do
         status = DEstimate.NEW,
         estimateBreakupList = estimateBreakupList',
         driversLocation = driversLocation,
-        nightShiftRate =
-          Just $
-            DEstimate.NightShiftRate
-              { nightShiftMultiplier = nightShiftRate >>= (.nightShiftMultiplier),
-                nightShiftStart = nightShiftRate >>= (.nightShiftStart),
-                nightShiftEnd = nightShiftRate >>= (.nightShiftEnd)
+        nightShiftInfo =
+          nightShiftInfo <&> \nightShiftInfo' ->
+            DEstimate.NightShiftInfo
+              { nightShiftCharge = nightShiftInfo'.nightShiftCharge,
+                oldNightShiftCharge = nightShiftInfo'.oldNightShiftCharge, -- TODO: Doesn't make sense, to be removed
+                nightShiftStart = nightShiftInfo'.nightShiftStart,
+                nightShiftEnd = nightShiftInfo'.nightShiftEnd
               },
         waitingCharges =
           DEstimate.WaitingCharges
-            { waitingChargePerMin = waitingCharges >>= (.waitingChargePerMin),
-              waitingTimeEstimatedThreshold = waitingCharges >>= (.waitingTimeEstimatedThreshold)
+            { waitingChargePerMin = waitingCharges >>= (.waitingChargePerMin)
             },
         ..
       }
@@ -214,7 +215,7 @@ buildQuote requestId providerInfo now merchantId QuoteInfo {..} = do
   tripTerms <- buildTripTerms descriptions
   quoteDetails' <- case quoteDetails of
     OneWayDetails oneWayDetails ->
-      pure . DQuote.OneWayDetails $ mkOneWayQuoteDetails oneWayDetails
+      pure.DQuote.OneWayDetails $ mkOneWayQuoteDetails oneWayDetails
     RentalDetails rentalSlab -> do
       DQuote.RentalDetails <$> buildRentalSlab rentalSlab
     OneWaySpecialZoneDetails details -> do

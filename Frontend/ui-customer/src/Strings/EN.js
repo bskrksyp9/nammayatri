bc4d09c613f4d969cc76fe6701adea2703959496
Aryan Chaudhary <aryan.chaudhary@juspay.in>
2023-05-12 15:13:27 +0530

    Fixed ML translations

--- Frontend/ui-customer/src/Strings/EN.js
+++ Frontend/ui-customer/src/Strings/EN.js
@@ -282,6 +282,7 @@ const englishStrings = {
   "SHARE_RIDE_WITH_EMERGENCY_CONTACTS": "Share Ride with Emergency Contacts",
   "DO_YOU_NEED_EMERGENCY_HELP": "Do you need Emergency Help ?",
   "CALL_SUPPORT": "Call Support",
+  "YOU_ARE_ABOUT_TO_CALL_YATRI_SUPPORT": "You are about to place a call to the Yatri Support Team. Do you want to proceed?",
   "YOU_ARE_ABOUT_TO_CALL_NAMMA_YATRI_SUPPORT": "You are about to place a call to the Namma Yatri Support Team. Do you want to proceed?",
   "YOU_ARE_ABOUT_TO_CALL_POLICE": "You are about to place a call to the Police. Do you want to proceed?",
   "DAIL_100": "Dial 100?",
